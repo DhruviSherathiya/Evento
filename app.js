6fdc2f8ebd92a848076e3560669bb2e5ce186b25
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2022-11-02 16:54:48 +0530

    Mongodb connected

--- app.js
+++ app.js
@@ -2,36 +2,72 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
-
+const mongoose = require('mongoose');
 const app = express();
-const events = [];
+const Event = require('./models/event');
 app.use(bodyParser.json());
 
 app.use (
     '/graphql', 
     graphqlHTTP({
         schema : buildSchema(`
-            
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+          
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
             
             type RootQuery {
-                events: [String!]!
+                events: [Event!]!
             }
             type RootMutation {
-                createEvent(name: String): String
+                createEvent(eventInput: EventInput): Event
             }
+
             schema {
                 query: RootQuery
                 mutation: RootMutation
             }
     `),
     rootValue : {
-        events: () => {
-            return ['Dhruvi'];
-        },
-        createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
-        } 
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      } 
     },
     graphiql: true,
     })
@@ -41,4 +77,15 @@ app.get('/',(req,res,next) => {
     res.send('Hello World');
 })
 
-app.listen(8000);
+mongoose
+.connect(`mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+  }@cluster0.fzfi3bl.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(8000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
