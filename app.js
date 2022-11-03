fd078828d6c4091dbcc88a071818f111376e8562
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2022-11-03 09:59:50 +0530

    React app created

--- app.js
+++ app.js
@@ -6,79 +6,15 @@ var { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 const app = express();
 const Event = require('./models/event');
-app.use(bodyParser.json());
+const isAuth = require('./middleware/is-auth');
 
 const graphQlSchema = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
 
- /** app.use (
-    '/graphql', 
-    graphqlHTTP({
-        schema : buildSchema(`
-            type Event {
-                _id: ID!
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-            }
-          
-            input EventInput {
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-            }
-            
-            type RootQuery {
-                events: [Event!]!
-            }
-            type RootMutation {
-                createEvent(eventInput: EventInput): Event
-            }
+app.use(bodyParser.json());
 
-            schema {
-                query: RootQuery
-                mutation: RootMutation
-            }
-    `),
-    rootValue : {
-      events: () => {
-        // pass arguments like title: 'Test' to get particular events but here we have not defined any arguments so it will return all
-        return Event.find()
-          .then(events => {
-            return events.map(event => {
-              return { ...event._doc, _id: event.id };
-            });
-          })
-          .catch(err => {
-            throw err;
-          });
-      },
-      createEvent: args => {
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date)
-        });
-        return event
-          // save will write our data into database
-          .save()
-          .then(result => {
-            console.log(result);
-            return { ...result._doc, _id: result._doc._id.toString() };
-          })
-          .catch(err => {
-            console.log(err);
-            throw err;
-          });
-      } 
-    },
-    graphiql: true,
-    })
-);
-**/ 
+app.use(isAuth);
+
 app.use(
   '/graphql',
   graphqlHTTP({
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -24,6 +24,11 @@ type User {
   password: String
   createdEvents: [Event!]
 }
+type AuthData {
+  userId: ID!
+  token: String!
+  tokenExpiration: Int!
+}
 input EventInput {
   title: String!
   description: String!
@@ -38,6 +43,7 @@ input UserInput {
 type RootQuery {
     events: [Event!]!
     bookings: [Booking!]!
+    login(email: String!, password: String!): AuthData!
 }
 
 type RootMutation {
