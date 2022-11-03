13339945036aa7d1f0556c4a753508c5008db6a3
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2022-11-03 11:44:07 +0530

    Navbar Added

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -6,19 +6,25 @@ import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Booking';
 import EventsPage from './pages/Events';
 import HomePage from './pages/Home';
+import ErrorPage from './pages/Error';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <MainNavigation />
+      <main className="main-content">
       <Routes>
         <Route path="/" element={<HomePage />}>
-          <Route index element={<AuthPage />} />
-          <Route path="bookings" element={<BookingsPage />} />
-          <Route path="event" element={<EventsPage />} />
-          <Route path="*" element={<null />} />
+        <Route path="auth" element={<AuthPage />} />
+        <Route path="bookings" element={<BookingsPage />} />
+        <Route path="events" element={<EventsPage />} />
+        <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
+      </main>
+      
     </BrowserRouter>
     </div>
   );
---
+++ frontend/app/src/pages/Error.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ErrorPage extends Component {
+  render() {
+    return <h1>Page Not Found</h1>;
+  }
+}
+
+export default ErrorPage;
