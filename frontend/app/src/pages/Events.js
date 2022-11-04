44d972292dee2c905fec668fcbef874960119c0a
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2022-11-04 10:55:36 +0530

    Modal Added

--- frontend/app/src/pages/Events.js
+++ frontend/app/src/pages/Events.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
-import '../index.css';
+
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
+import '../pages/Events.css';
 
 class EventsPage extends Component {
+  state = {
+    creating: false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({ creating: true });
+  };
+
+  modalConfirmHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  modalCancelHandler = () => {
+    this.setState({ creating: false });
+  };
+
   render() {
-    return <div>
-      <button className='btn'>Create Event</button>
-    </div>;
+    return (
+      <React.Fragment>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && (
+          <Modal
+            title="Add Event"
+            canCancel
+            canConfirm
+            onCancel={this.modalCancelHandler}
+            onConfirm={this.modalConfirmHandler}
+          >
+            <p>Modal Content</p>
+          </Modal>
+        )}
+        <div className="events-control">
+          <p>Share your own Events!</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>
+            Create Event
+          </button>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
--- frontend/app/src/pages/Profile.js
+++ frontend/app/src/pages/Profile.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import '../pages/Profile.css';
-
+import Modal from '../components/Modal/Modal';
 
 class HomePage extends Component {
+
+  state = {
+    creating: false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({ creating: true });
+  };
+
+  modalConfirmHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  modalCancelHandler = () => {
+    this.setState({ creating: false });
+  };
+
   render() {
     return (
     <div className='main-div'>
@@ -14,6 +31,17 @@ class HomePage extends Component {
             </div>
         </div>
         <div className='div2'>
+        {this.state.creating && (
+          <Modal
+            title="Add Event"
+            canCancel
+            canConfirm
+            onCancel={this.modalCancelHandler}
+            onConfirm={this.modalConfirmHandler}
+          >
+            <p>Modal Content</p>
+          </Modal>
+        )}
         <div className="events-control">
           <p>Share your own Events!</p>
           <button className="btn">
